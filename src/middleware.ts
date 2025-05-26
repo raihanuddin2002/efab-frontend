9f885155355f809c40dc6f728b725821a7e605ab
RAIHAN UDDIN <raihanuddin@RAIHANs-MacBook-Pro.local>
2025-05-27 02:40:37 +0600

    update session

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/update-session'
+
 export async function middleware(request: NextRequest) {
     return await updateSession(request)
 }
