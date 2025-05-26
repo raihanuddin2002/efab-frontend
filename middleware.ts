3add8222e42502892bf7adb2711ef0879bcc214e
RAIHAN UDDIN <raihanuddin@RAIHANs-MacBook-Pro.local>
2025-05-27 03:49:11 +0600

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/update-session'
 
 export async function middleware(request: NextRequest) {
+    console.log("middleware")
     return await updateSession(request)
 }
 export const config = {
