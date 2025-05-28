09a00d9836455f24369479e239a49c279d203be6
RAIHAN UDDIN <raihanuddin@RAIHANs-MacBook-Pro.local>
2025-05-28 14:11:16 +0600

    Dashboard User profile added

--- src/app/(AdminPanel)/dashboard/page.tsx
+++ src/app/(AdminPanel)/dashboard/page.tsx
@@ -1,9 +1,24 @@
+import { getSession } from '@/app/supabase-server-actions'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const session = await getSession()
+
     return (
         <section className='py-[50px]'>
-            <h1 className='text-3xl text-center font-bold'>Welcome to efab Admin Dashboard!</h1>
+            <h2 className='text-3xl text-center font-bold mb-10'>Welcome to efab Admin Dashboard!</h2>
+
+            {session && (
+                <div className='text-center'>
+                    <div className='w-[100px] h-[100px] rounded-full bg-slate-200 mx-auto mb-5 flex items-center justify-center'>
+                        <FontAwesomeIcon size='xl' className='text-slate-600' icon={faUser} />
+                    </div>
+                    <h4 className='text-4xl font-semibold'>{session.user?.user_metadata?.full_name}</h4>
+                    <p className='text-xl'>{session.user?.email}</p>
+                </div>
+            )}
         </section>
     )
 }
